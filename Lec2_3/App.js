f83c272b9aa3cbafe8f077bfbf1fec7719341a6c
JAI <jai.sharma2023a@gmail.com>
2024-05-29 18:16:10 +0530

    Removing the console log-babel, JSX, Functional Components

--- Lec2_3/App.js
+++ Lec2_3/App.js
@@ -18,37 +18,123 @@
 * 
 * PARSER HAS TRANSITIVE DEPENDENCIES (dependencies which depends on other dependecies which depends on other dependencies and so on)
 * "WE HAVE A PACKAGE MANAGER WHICH HANDLES AND TAKES CARE OF OUR TRANSITIVE DEPENDENCIES OF OUT CODE"
-* 
+* Tree Shaking - Remove Un-wanted
 * 
 */
+
+
+// babel - this is compiler for next generation javascript(React)
 
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Heading 1"
-);
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title",
-  },
-  "Heading 2"
+
+// const heading2 = React.createElement(
+//   "h2",
+//   {
+//     id: "title",
+//     key: "h2"
+//   },
+//   "Heading 2"
+// );
+
+// JSX => React.createElement => Object => HTML(DOM)
+// babel converts jsx to React.createElement
+
+
+{/* <div class="header">
+ <h1>Namaste React</h1>
+ <ul>
+    <li>About Us</li>
+    <li>Support</li>
+    <li>Home</li> 
+  </ul>
+</div>; */}
+// const container = React.createElement(
+//   "div",
+//   {
+//     id: "container",
+//   },
+//   [
+//     React.createElement(
+//       "h1",
+//       {},
+//       "Namaste React"
+//     ),
+//     React.createElement(
+//       "ul", {},
+//       [
+//         React.createElement("li", {}, "About Us"),
+//         React.createElement("li", {}, "Home"),
+//         React.createElement("li", {}, "Support"),
+//       ]
+//     ),
+//   ]
+// );
+
+// JSX ??? Javascript XML? No, but can say
+
+/*
+* Readability
+* Maintainabilty
+* Syntactical Sugar coat
+* Developer Experience
+* Less code
+* Not Repition
+*/
+
+
+
+// ReactElement - OBJECT
+
+
+// React Component
+// - Functional Components - NEW - Function
+// - Class Based Components - OLD (Imp for interview and life cycle methods)
+
+// THIS A REACT ELEMENT
+const heading1 = (
+  <h1 id="title" key="h1">
+    Namaste React
+  </h1>
+  // this is not html inside javascript, its html like syntax
+  // babel interprets this html like syntax(JSX) and then converts this into html code
 );
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading2]
+// FUNCTIONAL COMPONENTS
+
+// returns the JSX code or React element (both are same, no?)
+// name of component starts with the capital letter (Convention) - not mandatory but good practice to use
+
+// If it returns a JSX code then it becomes react component else it's just some jsx code
+
+// THIS IS REACT COMPONENT
+const Heading = () => (
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>
+);
+const HeaderComponent = function() {
+  return (
+    <>
+    
+      <Heading /> {/* Component Composition - Component inside a component*/}
+      {Heading()}
+      {heading1}
+      <h1>Namaste React Functional Components</h1>
+      <h2>This is a h2 tag</h2>
+    </>
+  );
+}
+const HeaderComponent1 = () =>  (
+  <>
+    <h1>Namaste React Functional Components</h1>
+    <h2>This is a h2 tag</h2>
+  </>
 );
 
-console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+
+root.render(<HeaderComponent />);
+// root.render(heading1);
